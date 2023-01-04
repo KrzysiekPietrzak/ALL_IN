747dd72e58c9d9359ed6832ff29b965204519645
KrzysiekPietrzak <krzysztof.pietrzak17@gmail.com>
2023-01-04 19:31:54 +0100

    New Year Cleanup - general improvements

---
+++ Nowy folder/CO/index.js
@@ -0,0 +1,120 @@
+const menu = [
+    {
+      id: 1,
+      title: "Mikstura przeciwpiechotna",
+      category: "Specjalne",
+      price: "xx.xx",
+      img: "./menu1.jpg",
+      desc: `Bechorovka i Schweppes`,
+    },
+    {
+      id: 2,
+      title: "30-1975",
+      category: "Specjalne",
+      price: "xx.xx",
+      img: "./menu2.jpg",
+      desc: `Whisky z Colą z plastikowego kubka`,
+    },
+    
+    {
+      id: 3,
+      title: "Sihill Zoltana Chivaya",
+      category: "Specjalne",
+      price: "xx.xx",
+      img: "./menu3.png",
+      desc: `Zestaw pięciu i trzech shotów`,
+    },
+    {
+      id: 4,
+      title: "Norweski zawrót głowy",
+      category: "Alchemia",
+      price: "xx.xx",
+      img: "./menu4.jpg",
+      desc: `Chłodziwo pół na pół z wodą`,
+    }, {
+      id: 5,
+      title: "Przysmak watykański",
+      category: "Słodko",
+      price: "21.37",
+      img: "./menu5.jpg",
+      desc: `Osłodzenie wieczoru kierowcy`,
+    },
+]
+
+//const buttons = document.getElementsByClassName("buttons");
+const offer = document.querySelector(".offer");
+const buttons = document.querySelector(".buttonsy");
+
+//console.log(offer.innerHTML);
+window.addEventListener("DOMContentLoaded", function () {
+
+    displayMenuItems(menu);
+    displayMenuButtons();
+
+   /* displayMenuButtons();
+*/  
+});
+  function displayMenuItems(menuItems) {
+    let displayMenu = menuItems.map(function (item) {
+      // console.log(item);
+  
+      return `<article class="menu-item">
+            <img src=${item.img} alt=${item.title} class="photo" />
+            <div class="item-info">
+              <header>
+                <h4 class="offer-title">${item.title}</h4>
+                <h4 class="price">${item.price} PLN</h4>
+              </header>
+              <p class="item-text">
+                ${item.desc}
+              </p>
+            </div>
+          </article>`;
+    });
+
+    displayMenu = displayMenu.join("");
+    //console.log(displayMenu);
+    offer.innerHTML = displayMenu;
+  }
+
+
+  function displayMenuButtons() {
+    const categories = menu.reduce(
+      function (values, item) {
+        if (!values.includes(item.category)) {
+          values.push(item.category);
+        }
+        return values;
+      },
+      ["Wszystko"]
+    );
+    const categoryBtns = categories
+      .map(function (category) {
+        return `<button type="button" class="buttons" data-id=${category}>
+            ${category}
+          </button>`;
+      })
+      .join("");
+  
+    buttons.innerHTML = categoryBtns;
+    const filterBtns = buttons.querySelectorAll(".buttons");
+    //console.log(filterBtns);
+  
+    filterBtns.forEach(function (btn) {
+      btn.addEventListener("click", function (e) {
+        // console.log(e.currentTarget.dataset);
+        const category = e.currentTarget.dataset.id;
+        const menuCategory = menu.filter(function (menuItem) {
+          // console.log(menuItem.category);
+          if (menuItem.category === category) {
+            return menuItem;
+          }
+        });
+        if (category === "Wszystko") {
+          displayMenuItems(menu);
+        } else {
+          displayMenuItems(menuCategory);
+        }
+      });
+    });
+  }
--- Nowy folder/all_in_reuse/script.js
+++ Nowy folder/all_in_reuse/script.js
@@ -1,3 +1,111 @@
+
 function btn(){
 window.location = "../all_in_reuse/second.html";
+}
+
+
+let Events =[{
+    id: 1,
+    title_name:"2010's",
+    day: "poniedziałek",
+    url:"../img(1).jpg",
+},{
+    id: 2,
+    title_name:"2000's",
+    day:"wtorek",
+    url:"../img(2).jpg",
+},{
+    id: 3,
+    title_name:"90's",
+    day:"wtorek",
+    url:"../img(3).jpg",
+},{
+    id: 4,
+    title_name:"Rap",
+    day:"środa",
+    url:"../img(4).jpg",
+},{
+    id: 5,
+    title_name:"Mongolski śpiew gardłowy",
+    day:"środa",
+    url:"../img(5).jpg",
+},{
+    id: 6,
+    title_name:"Muzyka klasyczna kompozytorów autriackich",
+    day:"czwartek",
+    url:"../img(6).jpg",
+},{
+    id: 7,
+    title_name:"Rock Alternatywny",
+    day:"piątek",
+    url:"../img(7).jpg",
+},{
+    id: 8,
+    title_name:"Folk",
+    day:"sobota",
+    url:"../img(8).jpg",
+},{
+    id: 9,
+    title_name:"Reggae",
+    day:"sobota",
+    url:"../img(9).jpg",
+},{
+    id: 10,
+    title_name:"Power Metal",
+    day:"niedziela",
+    url:"../img(10).jpg",
+}]
+
+window.addEventListener("DOMContentLoaded", function () {
+
+    displayEvents();
+
+
+});
+
+
+function closeModal(){
+    let modal1 = document.getElementById("modal1");
+    modal1.style.visibility="hidden";
+
+}
+function closeAdblock(){
+    let modal2 = document.getElementById("modal2");
+    modal2.style.visibility="hidden";
+}
+function closeCookies(){
+    let modal3 = document.getElementById("modal3");
+    modal3.style.visibility="hidden";
+}
+function closeNews(){
+    let modal4 = document.getElementById("modal4");
+//modal4.style.visibility="hidden";
+modal4.classList.remove("invisible2");
+}
+let isNewsletterModal = false;
+
+document.addEventListener('scroll', (e)=> {
+    if (!isNewsletterModal) modal4.classList.add("invisible2");
+isNewsletterModal=true;
+}
+)
+
+function displayEvents(){
+    let events= document.getElementById("events");
+    let displayE = Events.map(function (item){
+        console.log(item);
+        return `
+        <div class="event" id=${item.id}>
+            <div class="img-wrap">
+              <img class="img-event" src=${item.url} alt="">
+            </div><div class="two-text-event">
+        <p class="title-name">${item.title_name}</p>
+          <p class="p-event-hide">${item.day}</p>
+          </div>
+        </div>`
+          
+    });
+    displayE = displayE.join(``);
+    console.log(displayE);
+    events.innerHTML = displayE;
 }
